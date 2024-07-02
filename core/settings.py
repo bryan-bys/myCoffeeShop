e9a8ca600472e5a9a6ce3ef07a81ad1dfaa92de3
bryan-bys <bryanmm212@gmail.com>
2024-07-02 20:04:02 +0200

    settings cors modified

--- core/settings.py
+++ core/settings.py
@@ -154,5 +154,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
   'http://localhost:5173',
   'https://mycoffeeshop-zw5y.onrender.com',
-    
+   "https://myx-f3an.onrender.com",
 ]
